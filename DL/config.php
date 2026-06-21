506050c48acbfd27435ebb843066387a41784c24
Aina Gisbert <ainetagisbert@gmail.com>
2026-06-21 15:07:00 +0200

    Correct config variables

--- DL/config.php
+++ DL/config.php
@@ -4,7 +4,7 @@
 define("SERVERNAME", getenv("MYSQLHOST"));
 define("USERNAME",   getenv("MYSQLUSER"));
 define("PASSWORD",   getenv("MYSQLPASSWORD"));
-define("DBNAME",     getenv("MYSQLDATABASE"));
+define("DBNAME",     getenv("MYSQL_DATABASE"));
 define("PORT",       getenv("MYSQLPORT"));
 
 ?>
